f5b7e91c7125bca9ef21fb16023cd8e96dd09f7c
tckry <1150590475@qq.com>
2023-12-16 20:39:57 +0800

    feature: 开发全局异常拦截器

---
+++ admin/src/main/java/org/tckry/shortlink/admin/common/web/GlobalExceptionHandler.java
@@ -0,0 +1,74 @@
+package org.tckry.shortlink.admin.common.web;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
+import org.tckry.shortlink.admin.common.convention.errorcode.BaseErrorCode;
+import org.tckry.shortlink.admin.common.convention.exception.AbstractException;
+import org.tckry.shortlink.admin.common.convention.result.Result;
+import org.tckry.shortlink.admin.common.convention.result.Results;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
+/**
+ * 全局异常处理器
+ *
+ */
+@Component
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 拦截参数验证异常
+     */
+    @SneakyThrows
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)    // 拦截的类，如果是父类，所有的子类也会被拦截
+    public Result validExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        FieldError firstFieldError = CollectionUtil.getFirst(bindingResult.getFieldErrors());
+        String exceptionStr = Optional.ofNullable(firstFieldError)
+                .map(FieldError::getDefaultMessage)
+                .orElse(StrUtil.EMPTY);
+        log.error("[{}] {} [ex] {}", request.getMethod(), getUrl(request), exceptionStr);
+        return Results.failure(BaseErrorCode.CLIENT_ERROR.code(), exceptionStr);
+    }
+
+    /**
+     * 拦截应用内抛出的异常
+     */
+    @ExceptionHandler(value = {AbstractException.class})    // 拦截AbstractException的三个子类，Remote、Client、ServiceException
+    public Result abstractException(HttpServletRequest request, AbstractException ex) {
+        if (ex.getCause() != null) {
+            log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString(), ex.getCause());
+            return Results.failure(ex);
+        }
+        log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString());
+        return Results.failure(ex);
+    }
+
+    /**
+     * 拦截未捕获异常
+     */
+    @ExceptionHandler(value = Throwable.class)  // 顶级异常父类Throable，保证所有异常被捕获
+    public Result defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
+        log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
+        return Results.failure();
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        if (StringUtils.isEmpty(request.getQueryString())) {
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+}
