ae2ff94dc7337e24f9d4b50eaf2516fe1d5fb263
tckry <1150590475@qq.com>
2023-12-19 20:52:33 +0800

    feature: 用户登录拦截器忽略登录接口

--- admin/src/main/java/org/tckry/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/tckry/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -11,11 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 用户信息传输过滤器
  *
- * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
  */
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
@@ -24,13 +24,17 @@ public class UserTransmitFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-        if (userInfoJsonStr != null) {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);   // 将当前对象放到上下文
+        String requestURI = httpServletRequest.getRequestURI(); // url 加上路径域名的整体， uri只含后面部分/api/short-link/admin/v1/user/login
+        if (!Objects.equals(requestURI,"/api/short-link/v1/user/login")){
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userInfoJsonStr != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);   // 将当前对象放到上下文
+            }
         }
+
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
