53031b924d9883158cfdae8b80336cbf1c0d2f95
tckry <1150590475@qq.com>
2023-12-20 21:32:51 +0800

    feature: 提交Mysql数据库分页插件

---
+++ admin/src/main/java/org/tckry/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,25 @@
+package org.tckry.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Mybatis分页插件配置数据库类型，否则total为0
+ * @program: shortlink
+ * @description:
+ * @author: lydms
+ * @create: 2023-12-20 21:28
+ **/
+@Configuration
+public class DataBaseConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
