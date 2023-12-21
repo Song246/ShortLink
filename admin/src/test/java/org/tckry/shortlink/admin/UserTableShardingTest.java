a88e8b0dfc7fe77e350bac1f4a37404a59892089
tckry <1150590475@qq.com>
2023-12-21 15:40:27 +0800

    feature: SQL分表脚本生成语句

--- admin/src/test/java/org/tckry/shortlink/admin/UserTableShardingTest.java
+++ admin/src/test/java/org/tckry/shortlink/admin/UserTableShardingTest.java
@@ -22,9 +22,30 @@ public class UserTableShardingTest {
             "  UNIQUE INDEX `idx_unique_username`(`username`) USING BTREE COMMENT '用户名唯一索引'\n" +
             ") ENGINE = InnoDB AUTO_INCREMENT = 1736288012416491523 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;";
 
+    public static final String SQL2 ="CREATE TABLE `t_link_%d` (\n" +
+            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
+            "  `origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
+            "    `click_num` int(11) DEFAULT 0 COMMENT '点击量',\n" +
+            "    `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "    `favicon` varchar(256) DEFAULT NULL COMMENT '网站图标',\n" +
+            "    `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
+            "    `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
+            "    `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
+            "    `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+            "    `describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+
     public static void main(String[] args) {
         for (int i = 0; i < 16 ; i++) {
-            System.out.println(SQL.formatted(i));
+            System.out.println(SQL2.formatted(i));
         }
     }
 }
